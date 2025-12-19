2f5825de4f1a3e03ac85774d551f67de4b920809
bala-amypo <bala@amypo.in>
2025-12-19 09:48:19 +0000

    User commit

--- src/main/java/com/example/demo/repository/SkillGapRecommendationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.SkillGapRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SkillGapRecordRepository
-        extends JpaRepository<SkillGapRecord, Long> {
-}
