c54e8cb2218d172542d75d5e72f00aa2c881342e
bala-amypo <bala@amypo.in>
2025-12-20 15:17:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -6,22 +6,20 @@ import lombok.*;
 @Entity
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String fullName;
-
-    @Column(unique = true, nullable = false)
     private String email;
-
     private String password;
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    private boolean active = true;
 }
