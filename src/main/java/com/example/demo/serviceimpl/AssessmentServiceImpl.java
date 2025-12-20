3e4544cbf95279ba17c1b40027f20759a8263f00
bala-amypo <bala@amypo.in>
2025-12-20 11:21:07 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/AssessmentServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.serviceimpl;
-
-import org.springframework.stereotype.Service;
-import com.example.demo.service.AssessmentService;
-
-@Service
-public class AssessmentServiceImpl implements AssessmentService {
-    // implement methods here
-}
