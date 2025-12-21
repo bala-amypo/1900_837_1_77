a17a03ee1d0c5beab4849c686afd62a34d6e05b9
bala-amypo <bala@amypo.in>
2025-12-21 12:34:02 +0000

    User commit

--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -1,8 +1,36 @@
-import lombok.Data;
+package com.example.demo.dto;
 
-@Data
+import com.example.demo.entity.Role;
+
 public class RegisterRequest {
+
     private String username;
     private String password;
-    private String role; // STRING (converted later)
+    private Role role;
+
+    public RegisterRequest() {}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
