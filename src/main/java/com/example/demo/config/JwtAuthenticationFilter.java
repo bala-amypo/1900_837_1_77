ed62cf39135f244dbd9a57b523ddd3e0757d8d12
bala-amypo <bala@amypo.in>
2025-12-21 12:37:33 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
@@ -1,12 +1,21 @@
+package com.example.demo.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
     private JwtUtil jwtUtil;
-
-    public JwtAuthenticationFilter() {
-        // REQUIRED by Spring
-    }
 
     @Override
     protected void doFilterInternal(
