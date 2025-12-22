15e58a31a406dad7e90c24496ee38c44624b7e33
bala-amypo <bala@amypo.in>
2025-12-22 12:42:43 +0000

    User commit

---
+++ src/main/java/com/example/demo/serviceimpl/StudentProfileServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.demo.serviceimpl;
+
+import com.example.demo.entity.StudentProfile;
+import com.example.demo.repository.StudentProfileRepository;
+import com.example.demo.service.StudentProfileService;
+
+public class StudentProfileServiceImpl implements StudentProfileService {
+
+    private final StudentProfileRepository repository;
+
+    public StudentProfileServiceImpl(StudentProfileRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public StudentProfile createOrUpdateProfile(StudentProfile profile) {
+        return repository.save(profile);
+    }
+
+    @Override
+    public StudentProfile getByUserId(Long userId) {
+        return repository.findByUserId(userId)
+                .orElseThrow(() -> new RuntimeException("profile not found"));
+    }
+}
