f8c13462aa98f66277343e365d186acb3e1a06f4
bala-amypo <bala@amypo.in>
2025-12-22 12:54:43 +0000

    User commit

---
+++ src/main/java/com/example/demo/servlet/ServiceTestController.java
@@ -0,0 +1,21 @@
+package com.example.demo.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/test")
+public class ServiceTestController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().write("OK");
+    }
+}
