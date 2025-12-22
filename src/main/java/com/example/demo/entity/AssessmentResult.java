7b94b0f0d4f2f2c86231ab14fe31d493a565d26d
bala-amypo <bala@amypo.in>
2025-12-22 17:41:54 +0000

    User commit

--- src/main/java/com/example/demo/entity/AssessmentResult.java
+++ src/main/java/com/example/demo/entity/AssessmentResult.java
@@ -1,74 +1,26 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.Instant;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AssessmentResult {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Double score;
+    private Long studentProfileId;
+    private Long skillId;
+    private double score;
+    private double maxScore;
 
-    private Double maxScore;
-
-    @ManyToOne
-    private Skill skill;
-
-    @ManyToOne
-    private StudentProfile studentProfile;
-
-    private Instant assessedAt;
-
-    public AssessmentResult() {}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Double getScore() {
-        return score;
-    }
-
-    public void setScore(Double score) {
-        this.score = score;
-    }
-
-    public Double getMaxScore() {
-        return maxScore;
-    }
-
-    public void setMaxScore(Double maxScore) {
-        this.maxScore = maxScore;
-    }
-
-    public Skill getSkill() {
-        return skill;
-    }
-
-    public void setSkill(Skill skill) {
-        this.skill = skill;
-    }
-
-    public StudentProfile getStudentProfile() {
-        return studentProfile;
-    }
-
-    public void setStudentProfile(StudentProfile studentProfile) {
-        this.studentProfile = studentProfile;
-    }
-
-    public Instant getAssessedAt() {
-        return assessedAt;
-    }
-
-    public void setAssessedAt(Instant assessedAt) {
-        this.assessedAt = assessedAt;
-    }
+    private Instant attemptedAt;
 }
