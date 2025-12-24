ed83820be90ace7982f97d8e6efbd78332d2c637
bala-amypo <bala@amypo.in>
2025-12-24 05:28:04 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/SkillServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/SkillServiceImpl.java
@@ -18,9 +18,16 @@ public class SkillServiceImpl implements SkillService {
     }
 
     @Override
-    public Skill createSkill(Skill skill) {
-        return repo.save(skill);
+public Skill createSkill(Skill skill) {
+    if (skillRepository.findByCode(skill.getCode()).isPresent()) {
+        throw new IllegalArgumentException("Duplicate skill code");
     }
+    if (skill.getActive() == null) {
+        skill.setActive(true); // also fixes t016
+    }
+    return skillRepository.save(skill);
+}
+
 
     @Override
     public Skill updateSkill(Long id, Skill updated) {
